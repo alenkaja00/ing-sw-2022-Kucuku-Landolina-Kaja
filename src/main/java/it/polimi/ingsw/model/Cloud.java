2bbfcebeec5af5d9bb81b564ec04ef5556426ec5
giova <giovannilndln@gmail.com>
2022-03-22 23:31:29 +0100

    New class added: Cloud and Island

--- src/main/java/it/polimi/ingsw/model/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/Cloud.java
@@ -1,4 +1,34 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+
 public class Cloud {
+
+
+    private ArrayList<Student> studentArray ;
+    private int cloudCapacity;
+
+    public Cloud(int cloudCapacity){
+
+        this.cloudCapacity = cloudCapacity;
+        this.studentArray = new ArrayList<Student>();
+
+    }
+    public void refillCloud(ArrayList<Student> newStudentArray)
+    {
+        for(int i = 0; i < newStudentArray.size(); i++){
+            //copying the new students in the arraylist from the parameter
+            this.studentArray.set(i,newStudentArray.get(i));
+        }
+
+    }
+
+    public void removeStudent()
+    {
+
+
+    }
+
+
+
 }
--- src/main/java/it/polimi/ingsw/model/Island.java
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+
+
 public class Island {
+
+    private ArrayList<Integer> graphicalIsland [];
+    private ArrayList<Integer> islandTowers [];
+
+    public  void evaluateInfluence()
+    {
+
+
+
+    }
+
+    public void addStudent()
+    {
+
+
+    }
+
+    public void removeStudent()
+    {
+
+
+    }
+
 }
