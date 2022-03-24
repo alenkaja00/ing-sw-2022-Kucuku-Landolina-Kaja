314b85f707f538f89f07995750ecf8c5679b96dc
Alen Kaja <alen.kaja00@gmail.com>
2022-03-24 22:03:51 +0100

    Deck, Wizard, HelperCard

--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -7,34 +7,36 @@ import java.util.ArrayList;
 
 public class Deck {
     private Wizard wizardID;
-    private ArrayList<HelperCard> deck = new ArrayList<HelperCard>(10);
+    private ArrayList<HelperCard> deck = new ArrayList<HelperCard>();
 
 
-    public Deck() {
+    public Deck(Wizard wizardID) {
+        this.wizardID=wizardID;
         for(int i=0;i<10;i++)
         {
-            deck.get(i).setUsed(false);
-            deck.get(i).setMaxMoves(motherNatureMoves(i));
+            deck.add(new HelperCard(i+1,i/2+1));
         }
     }
+
     //calculate mother nature moves given the card
     public int motherNatureMoves(int index){
         return index/2+1;
     }
-    //return the remaining cards of the player
 
+    public HelperCard useCard(int cardNum)
+    {
 
-    public HelperCard useCard(){
     }
 
-
-
-
-    public ArrayList<HelperCard> returnCards(){
-        ArrayList<HelperCard> remainingCards= new ArrayList<HelperCard>();
-        for(int i=0;i<deck.size();i++){
-            remainingCards.add(deck.get(i));
-        }
-        return remainingCards;
+    public ArrayList<HelperCard> returnCards()
+    {
+        return (ArrayList<HelperCard>) deck.clone();
+    }
+    public Wizard getWizardID()
+    {
+        return wizardID;
+    }
+    public void setWizardID(Wizard wizardID) {
+        this.wizardID = wizardID;
     }
 }
