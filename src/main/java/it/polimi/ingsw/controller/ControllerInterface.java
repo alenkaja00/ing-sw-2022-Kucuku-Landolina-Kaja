dddcec4280cad547acbab75a6fae88eb7491eb53
giova <giovannilndln@gmail.com>
2022-03-31 18:43:28 +0200

    Skeleton of Controller

---
+++ src/main/java/it/polimi/ingsw/controller/ControllerInterface.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.controller;
+
+public interface ControllerInterface
+{
+
+
+
+}
---
+++ src/main/java/it/polimi/ingsw/controller/FinalState.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.controller;
+
+public class FinalState extends AbstractState
+{
+
+
+
+}
