985c2e7d11fbfa326352d099448283c02b942b16
Alen Kaja <alen.kaja00@gmail.com>
2022-05-06 18:03:46 +0300

    gson pretty printing

--- src/main/java/it/polimi/ingsw/server/App.java
+++ src/main/java/it/polimi/ingsw/server/App.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.server;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import it.polimi.ingsw.client.view.cli.utils.ANSIColor;
 import it.polimi.ingsw.server.controller.ClientManager;
 import it.polimi.ingsw.server.controller.ServerNetwork;
@@ -17,8 +18,8 @@ public class App
 
     public static void main( String[] args ) throws IOException {
 
-        ArrayList<String> nickNames = new ArrayList<String>();
-        ArrayList<Wizard> wiz = new ArrayList<Wizard>();
+        ArrayList<String> nickNames = new ArrayList<>();
+        ArrayList<Wizard> wiz = new ArrayList<>();
         nickNames.add("endi");
         nickNames.add("giovanni");
         wiz.add(Wizard.WIZARD1);
@@ -26,7 +27,7 @@ public class App
         GameClass game = new GameClass("asd", 2, nickNames, wiz );
         //----end of Setup ------
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String result = gson.toJson(game);
         System.out.println(result);
 
